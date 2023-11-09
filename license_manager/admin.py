9471140c507298e00829c73249902fe2560bb019
amirhosseinhjz <amirh.hejazian@digikala.com>
2023-11-09 20:20:51 +0330

    fix license key field

--- license_manager/admin.py
+++ license_manager/admin.py
@@ -6,6 +6,6 @@ class UserActivationAdmin(admin.ModelAdmin):
     list_display = ('phone_number', 'license_key', 'device_id', 'expiration_date')
     search_fields = ('phone_number', 'license_key', 'device_id')
     list_filter = ('expiration_date',)
-
+    readonly_fields = ('license_key',)
 
 admin.site.register(models.UserActivation, UserActivationAdmin)
